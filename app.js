0bd6843398587a67b208c3d0e17825d8d5800629
Mahmoud AbouDeghedy <mahmoudehab10002gmail.com>
2021-12-29 00:49:05 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // app.use(express.static(`${__dirname}/public`));
+
+app.use(cors());
+
+app.options('*', cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet({ contentSecurityPolicy: false }));
