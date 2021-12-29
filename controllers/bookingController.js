dc82db7e5fd2450e621afebeefdb1e4fbb27b17d
Mahmoud AbouDeghedy <mahmoudehab10002gmail.com>
2021-12-29 16:10:33 +0200

    Improved stripe implementation2
    Improved stripe implementation 2#
    
    Improved stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,7 +21,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
@@ -47,7 +49,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
