38cd209760921d75bd5d9029e108865f4cfee591
Mahmoud AbouDeghedy <mahmoudehab10002gmail.com>
2021-12-30 00:25:42 +0200

    trying

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,7 +47,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async session => {
-  // console.log(session);
+  console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.object.amount_total / 100;
