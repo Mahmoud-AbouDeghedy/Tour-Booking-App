ce04a6ee76e7af2a5e48ecf3d0b59a73782253dd
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-05-02 01:23:20 +0300

    hi

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,7 +57,7 @@ const createBookingCheckout = async (session) => {
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
-  console.log(signature);
+  console.log('hi');
   let event;
   try {
     event = stripe.webhooks.constructEvent(
