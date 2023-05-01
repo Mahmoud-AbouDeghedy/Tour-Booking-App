40490fba2f6a412c3030580263546ebd5ff38f5c
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-05-01 23:24:46 +0300

    webhooks2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,7 +25,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    // cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
 
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     payment_method_types: ['card'],
