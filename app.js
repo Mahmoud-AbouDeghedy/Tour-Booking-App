a0c353b5833700dba610e6c5b08be886bb49c91e
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-05-01 19:35:11 +0300

    disable csp

--- app.js
+++ app.js
@@ -31,21 +31,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cors());
 
 app.options('*', cors());
-//app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'", '*'],
-      },
-    },
-  })
-);
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
